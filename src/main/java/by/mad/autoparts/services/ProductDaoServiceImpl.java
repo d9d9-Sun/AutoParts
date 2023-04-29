1e05c284ae8f833a5be902fab7b6040692abd338
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2023-04-29 22:10:58 +0300

    change some implementations

--- src/main/java/by/mad/autoparts/services/ProductDaoServiceImpl.java
+++ src/main/java/by/mad/autoparts/services/ProductDaoServiceImpl.java
@@ -1,35 +1,45 @@
 package by.mad.autoparts.services;
 
 import by.mad.autoparts.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductDaoServiceImpl implements ProductDaoService {
-    private static List<Product> productList = new ArrayList<>();
-    private static long id = 0L;
 
-    static {
-        productList.add((new Product(++id, "TRW", "Тормозные колодки", "Подходит для Volvo XC90", 250.00)));
-        productList.add((new Product(++id, "Patron", "Насос гидроусилителя", "Подходит для Volvo XC90", 180.00)));
-    }
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Override
     public List<Product> getAllProducts() {
-        return productList;
+        List<Product> products = jdbcTemplate.query("SELECT * FROM products", (ResultSet rs, int rownum) -> {
+            Product product = new Product();
+
+            product.setProductId(rs.getLong("product_id"));
+            product.setProductVendor(rs.getString("product_vendor"));
+            product.setProductName(rs.getString("product_name"));
+            product.setProductDescription(rs.getString("product_description"));
+            product.setProductPrice(rs.getDouble("product_price"));
+
+            return product;
+        });
+        return products;
     }
 
     @Override
     public void addNewProduct(Product product) {
-        product.setId(++id);
         productList.add(product);
+
     }
 
     @Override
-    public void deleteProductById(long id) {
-        productList.removeIf(productList -> productList.getId() == id);
+    public void deleteProductById(Long id) {
+        productList.removeIf(productList -> productList.getProductId() == id);
     }
 
 }
