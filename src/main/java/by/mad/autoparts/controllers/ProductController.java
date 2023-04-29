c69fa87a6eebacbb88281b58b86c40ce17ab1272
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2023-04-29 22:07:28 +0300

    add new methods to ProductController

--- src/main/java/by/mad/autoparts/controllers/ProductController.java
+++ src/main/java/by/mad/autoparts/controllers/ProductController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,5 +23,22 @@ public class ProductController {
         List<Product> productList = productDaoService.getAllProducts();
         model.addAttribute("productList", productList);
         return "catalog";
+    }
+
+    @GetMapping("/catalog/add-new-product")
+    public String showAddNewProductForm(@ModelAttribute("product") Product product) {
+        return "add-new-product";
+    }
+
+    @PostMapping("/catalog/add-new-product")
+    public String addNewProduct(@RequestParam String productVendor,
+                                @RequestParam String productName,
+                                @RequestParam String productDescription,
+                                @RequestParam double productPrice,
+                                @ModelAttribute("product") Product product) {
+
+        Product newProduct = new Product(productVendor, productName, productDescription, productPrice);
+        productDaoService.addNewProduct(newProduct);
+        return "redirect:/catalog";
     }
 }
