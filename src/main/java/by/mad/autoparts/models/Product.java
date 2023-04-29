38a2d3df9d8a74c066a2b4d1744ba47bff1c5ca3
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2023-04-29 22:08:27 +0300

    add new annotaions for db

--- src/main/java/by/mad/autoparts/models/Product.java
+++ src/main/java/by/mad/autoparts/models/Product.java
@@ -1,14 +1,38 @@
 package by.mad.autoparts.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
-    private long id;
-    private String vendor;
-    private String name;
-    private String description;
-    private double price;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "product_vendor")
+    private String productVendor;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_description", columnDefinition = "text")
+    private String productDescription;
+
+    @Column(name = "product_price")
+    private double productPrice;
+
+    public Product(String productVendor, String productName, String productDescription, double productPrice) {
+        this.productVendor = productVendor;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+    }
 }
