775d690fc67c38f8d00f5085b20c551a86d41b3b
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2023-05-02 02:47:50 +0300

    add new method

--- src/main/java/by/mad/autoparts/AutopartsApplication.java
+++ src/main/java/by/mad/autoparts/AutopartsApplication.java
@@ -1,13 +1,25 @@
 package by.mad.autoparts;
 
+import by.mad.autoparts.storage.StorageProperties;
+import by.mad.autoparts.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class AutopartsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutopartsApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.init();
+		};
+	}
 }
