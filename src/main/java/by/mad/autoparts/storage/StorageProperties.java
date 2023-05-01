b62747dd33385185d65bbc4c17908cd0b4026478
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2023-05-02 02:56:44 +0300

    add StorageProperties

---
+++ src/main/java/by/mad/autoparts/storage/StorageProperties.java
@@ -0,0 +1,16 @@
+package by.mad.autoparts.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+    private String location = "uploads";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
