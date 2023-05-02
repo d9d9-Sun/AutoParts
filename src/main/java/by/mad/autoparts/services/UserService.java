1870e2fac9583f53b3a027e43ca51060a725843e
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2023-05-02 17:46:12 +0300

    remove unnecessary methods

--- src/main/java/by/mad/autoparts/services/UserService.java
+++ src/main/java/by/mad/autoparts/services/UserService.java
@@ -1,13 +1,10 @@
 package by.mad.autoparts.services;
 
-import by.mad.autoparts.models.Admin;
-import by.mad.autoparts.models.UserRole;
+import by.mad.autoparts.models.Users;
 import by.mad.autoparts.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.security.Principal;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +12,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public boolean createUser(Admin user) {
+    public boolean createUser(Users user) {
         if (userRepository.findByUsername(user.getUsername()) != null) {
             return false;
         }
@@ -24,20 +21,4 @@ public class UserService {
         userRepository.save(user);
         return true;
     }
-
-    public Admin getAdminByPrincipal(Principal principal) {
-        if (principal != null) {
-            return userRepository.findByUsername(principal.getName());
-        }
-        return new Admin();
-    }
-
-    public UserRole getAdminUserRole(Principal principal) {
-        return userRepository.findByUsername(principal.getName())
-                .getUserRoles()
-                .stream()
-                .findFirst()
-                .orElseThrow();
-    }
-
 }
